17c98488edda0008f55fb3c8676d4d1a77f66dbe
John Franke <johnrfrankejr@gmail.com>
2020-04-18 13:30:50 -0500

    updated server to deploy to heroku

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds235947.mlab.com:35947/heroku_jzhbjqjs", {
+  useNewUrlParser: true
+});
+
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/mongoheadlines",
   {
